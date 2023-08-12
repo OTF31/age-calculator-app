4e521525051913f8251fbd1c383cc7d333c05d1f
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-08-12 13:51:03 -0500

    add desktop design

--- src/app/_components/Result/Result.tsx
+++ src/app/_components/Result/Result.tsx
@@ -6,19 +6,19 @@ const Result = () => {
   return (
     <Stack gap={0}>
       <Text classNames={{ root: classes.result }} truncate>
-        99{' '}
+        - -{' '}
         <Text span inherit>
           years
         </Text>
       </Text>
       <Text classNames={{ root: classes.result }} truncate>
-        12{' '}
+        - -{' '}
         <Text span inherit>
           months
         </Text>
       </Text>
       <Text classNames={{ root: classes.result }} truncate>
-        31{' '}
+        - -{' '}
         <Text span inherit>
           days
         </Text>
